99f5d27005147c1f5fe39b0d653737aa251a3c5b
[cwhit154] <cwhit154@uncc.edu>
2021-09-24 10:55:44 -0400

    atempting to add Project folder

---
+++ exercise2/app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 8080;
+const host = 'localhost';
+const server = http.createServer((req,res) => {
+    res.statusCode =  200;
+    res.setHeader('Contenet-Type', 'text/plain');
+
+    let path = './views/';
+    
+    if(req.url === '/' || req.url === '/contact'){
+        path = path + 'contact.html'
+    }else if(req.url === '/about.html' || req.url === '/about'){
+        path = path + 'about.html'
+    }else{
+        res.statusCode = 404;
+        path = path + '404.html'
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+
+    });
+});
+
+server.listen(port,host, () => {
+    console.log('The server is running on port ' , port)
+});
